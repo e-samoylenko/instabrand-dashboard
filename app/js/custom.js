ef2d9c702cc3b5937b2a9bcc4b08640783e58d32
Eugene <eugene.samoylenko@gmail.com>
2015-05-04 19:08:03 +0300

    Added some responsiveness

--- app/js/custom.js
+++ app/js/custom.js
@@ -17,6 +17,12 @@ jQuery(document).ready(function($) {
             }
         }
     });
+
+    $('#wrapper').attr('class', 'page-'+bodyClass());
+
+    $(window).resize(function() {
+    	$('#wrapper').attr('class', 'page-'+bodyClass());
+    });
     
     
 
@@ -35,8 +41,17 @@ var showLikers = function() {
 	$('#main-content').html('');
 	$('#tmp').load('./data/instabrand-data.htm tbody', function() {
 		$('#tmp tr:first, #tmp th').remove();
-		$('#likers-table').append($('#tmp').html()).detach().appendTo('#main-content').show();
+		$('#likers-table table').append($('#tmp').html());
+		$('#likers-table').detach().appendTo('#main-content').show();
 		$('#tmp').remove();
 	});
+}
 
+var bodyClass = function() {
+	var wdth = $(window).width();
+
+	if(wdth < 768) return 'xs';
+	else if(wdth < 992) return 'sm';
+	else if(wdth < 1200) return 'md';
+	else return 'lg';
 }
